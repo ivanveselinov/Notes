1ae0161a41270282586f4aa15407a18e248a7480
Ivan <ivanveselinov22@gmail.com>
2023-04-28 10:48:48 +1000

    Update Method added and tested with PostMan

--- handler.js
+++ handler.js
@@ -2,7 +2,15 @@
 
 module.exports.createNote = async (event) => {
   return {
-    statusCode: 200,
+    statusCode: 201,
     body: JSON.stringify("A new note created!")
+  };
+};
+
+module.exports.updateNote = async (event) => {
+  let notesId = event.pathParameters.id;
+  return {
+    statusCode: 200,
+    body: JSON.stringify(`The note with ${notesId} has been updated!`)
   };
 };
