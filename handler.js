8cde0892ab8a8822f5fffdf1623c486d9d3276a9
Ivan <ivanveselinov22@gmail.com>
2023-05-03 20:27:28 +1000

    Update Inserted inside Lambda and server + made send const

--- handler.js
+++ handler.js
@@ -3,13 +3,21 @@
 // Setup to read from AWS dunamoDb
 const DynamoDb = require("aws-sdk/clients/dynamodb");
 const documentClient = new DynamoDb.DocumentClient({ region: "ap-southeast-2" });
+const NOTES_TABLE_NAME = process.env.NOTES_TABLE_NAME; // Fetch NOTES_TABLE_NAME FROM serverless.yml !!
+
+const send = (statusCode, data) => {
+  return {
+    statusCode,
+    body: JSON.stringify(data)
+  };
+};
 
 module.exports.createNote = async (event, context, callback) => {
   let data = JSON.parse(event.body);
 
   try {
     const params = {
-      TableName: "notes", // Get table from DynamoDb AWS
+      TableName: NOTES_TABLE_NAME, // Get table from DynamoDb AWS
       Item: {
         notesId: data.id, // ID
         title: data.title, // Title
@@ -18,26 +26,40 @@ module.exports.createNote = async (event, context, callback) => {
       ConditionExpression: "attribute_not_exists(notesId)" // If condition ( data.id === data.id ) { error }
     };
     await documentClient.put(params).promise(); // Promice
-    callback(null, {
-      // When everything successfully is done we are doing callBack function
-      statusCode: 201,
-      body: JSON.stringify(data)
-    });
+    callback(null, send(201, data));
   } catch (err) {
-    callback(null, {
-      // If its not successfully callBack function
-      statusCode: 500, // Server Error
-      body: JSON.stringify(err.message) //Error message
-    });
+    callback(null, send(500, err.message));
   }
 };
 
-module.exports.updateNote = async event => {
+module.exports.updateNote = async (event, context, cb) => {
   let notesId = event.pathParameters.id;
-  return {
-    statusCode: 200,
-    body: JSON.stringify(`The note with ${notesId} has been updated!`)
-  };
+  let data = JSON.parse(event.body);
+
+  try {
+    const params = {
+      TableName: NOTES_TABLE_NAME,
+      Key: {
+        notesId: notesId
+      },
+      UpdateExpression: "set #title = :title, #body = :body", // Set title and body to the value set down to it
+      ExpressionAttributeNames: {
+        // Define Names
+        "#title": "title",
+        "#body": "body"
+      },
+      ExpressionAttributeValues: {
+        // Define Data
+        ":title": data.title,
+        ":body": data.body
+      },
+      ConditionExpression: "attribute_exists(notesId)" // Update only if item is available
+    };
+    await documentClient.update(params).promise(); // Promise that all is good
+    cb(null, send(200, data));
+  } catch (err) {
+    cb(null, send(500, err.message));
+  }
 };
 
 module.exports.deleteNote = async event => {
